ddad0f2e8a2bf8288ba37c6bc035c669f6213044
Skiddie <domenp93@gmail.com>
2021-09-02 18:48:17 +0200

    proof of concept

---
+++ js/callGit.js
@@ -0,0 +1,51 @@
+const display = document.getElementById('display')
+
+async function fetchRepo() {
+  let get = await fetch("https://api.github.com/users/skiddie93/repos")
+  let data = await get.json();
+
+  return data
+}
+
+async function fetchLangs(repos) {
+  let langData = []
+  for (item in repos) {
+
+    fetchStr = repos[item].languages_url
+    let get = await fetch(fetchStr)
+    let data = await get.json();
+    langData.push(data)
+
+  }
+  return langData
+}
+
+async function render(lang) {
+  let allLangs = {
+    sum: 0
+  }
+  lang.forEach(obj => {
+    for (atr in obj) {
+      allLangs.sum += obj[atr]
+      if (atr in allLangs) {
+        allLangs[atr] += obj[atr]
+      } else {
+        allLangs[atr] = obj[atr]
+      }
+    }
+  })
+  for (atr in allLangs) {
+    if (atr != "sum") {
+      display.innerHTML += "<div class='item'><p>" + atr + "</p><p>" + Math.round((100 * allLangs[atr] / allLangs.sum) * 10) / 10 + "%</p></div>"
+    }
+  }
+}
+
+
+async function processData() {
+  let data = await fetchRepo()
+  let langsData = await fetchLangs(data)
+  render(langsData)
+}
+
+processData()
